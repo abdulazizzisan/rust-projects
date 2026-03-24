047c630e634ace72f92c9d911d5d6732a9f44cf4
abdulazizizisan <binabdulaziz4983@gmail.com>
2026-03-24 20:21:20 +0600

    add mutation of struct instance

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -17,6 +17,20 @@ fn main() {
         active: false,
     };
 
-    println!("User created using regular method: \n {:#?}", user) // print user formatted.
+    println!("User created using regular method: \n {:#?}", user); // print user formatted.
+    println!();
 
+    let mut mutable_user = User{
+        name: "Md Abdul Aziz Zisan".to_string(),
+        username: "zisan".to_string(),
+        email: "hello@zisan.dev".to_string(),
+        password: "zisanzisan".to_string(),
+        active: false,
+    };
+
+    println!("Mutable user before mutating: \n {:#?}", mutable_user);
+
+    mutable_user.username = "zisan_mutated".to_string();
+
+    println!("Mutable user after mutating: \n {:#?}", mutable_user);
 }
